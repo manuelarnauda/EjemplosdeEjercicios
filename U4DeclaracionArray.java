179e7c88362c336b8b0b521538d8b3d7a497124c
Manuel Arnauda <143724150+manuelarnauda@users.noreply.github.com>
2023-10-17 09:39:31 -0500

    Add files via upload

---
+++ U4DeclaracionArray.java
@@ -0,0 +1,42 @@
+
+package u4.declaracionarray;
+
+
+public class U4DeclaracionArray {
+
+    
+    public static void main(String[] args) {
+       
+        int[] numeros = {1,2,3,4,5,6,7,8};
+        String[] nombres = {"Manuel", "Hugo", "Santi", "Jaziel", "Daniel", "Messi", "CR7", "Sofi", "Tere", "Javier", "Pablo", "Brenda", "Emi"};
+    
+    
+        boolean[] banderas;
+        char[] vocales;
+        double[] precios;
+        
+        System.out.println("longitud" + nombres.length);
+        System.out.println("elemento 3- posicion 2: " + nombres[2]);
+        System.out.println("elemento 10- posicion 9: " + nombres[9]);
+        System.out.println("elemento 5- posicion 4: " + nombres[4]);
+        System.out.println(nombres);
+        
+        System.out.println(nombres[0]);
+        System.out.println(nombres[1]);
+        System.out.println(nombres[2]);
+        System.out.println(nombres[3]);
+        System.out.println(nombres[4]);
+        System.out.println(nombres[5]);
+        System.out.println(nombres[6]);
+        System.out.println(nombres[7]);
+        System.out.println(nombres[8]);
+        System.out.println(nombres[9]);
+        System.out.println(nombres[10]);
+        System.out.println(nombres[11]);
+        System.out.println(nombres[12]);
+        System.out.println(nombres[13]);
+    
+    }
+    
+
+}
