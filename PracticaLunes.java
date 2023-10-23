8ce8f29cc21d8689ecfc44a80b9ad8272e7f15ef
Manuel Arnauda <143724150+manuelarnauda@users.noreply.github.com>
2023-10-23 07:49:45 -0600

    Add files via upload

---
+++ PracticaLunes.java
@@ -0,0 +1,32 @@
+
+package practicalunes;
+import java.util.Scanner;
+
+
+public class PracticaLunes {
+   
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        char valor = '*';
+        System.out.println("Cuantas filas desea observar? ");
+        int filas = scanner.nextInt();
+        
+        scanner.close();
+        
+            for (int i = 0; i < filas; i++) {
+                System.out.println(valor);
+                
+               
+                
+                
+            }
+            
+        }
+            
+        }
+        
+    
+    
+
